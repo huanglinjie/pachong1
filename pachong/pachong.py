67e6d2c27d61f714ff9654a5858ab26f200ad5be
Administrator <1396880935@qq.com>
2020-11-20 01:32:56 +0800

    更新代码

--- pachong/pachong.py
+++ pachong/pachong.py
@@ -9,8 +9,18 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
+#声明一个请求头
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"
 }
-response = requests.get("https://music.163.com/#/playlist?id=5028355122",headers=headers)
-print(response.text)
+# response = requests.get("https://music.163.com/#/playlist?id=5028355122",headers=headers)
+#输入的url
+play_url = input("请输入你想要爬取的歌单地址：")
+#保持会话
+s = requests.session()
+#声明响应变量
+response = s.get(play_url,headers=headers).content
+#实例化网页选择器
+soup = BeautifulSoup(response,'lxml')
+main = soup.find('ul',{'class':'f-hide'})
+print(main)
